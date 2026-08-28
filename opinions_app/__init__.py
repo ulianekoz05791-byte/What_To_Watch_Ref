65f251333151880bff6ea13711425a82c6d24383
Julia Nekoz <yulyanekoz@yandex.ru>
2026-08-29 02:18:51 +0300

    правка

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -1,17 +1,22 @@
-from opinions_app import models, forms, views, error_handlers, cli_commands
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from settings import Config
 from .extensions import db
 
-
 migrate = Migrate()
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db.init_app(app)
-migrate.init_app(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-# Импорты в конце, чтобы избежать циклических зависимостей
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Импорты после создания app
+    from . import views, error_handlers, cli_commands
+    
+    # Регистрируем blueprint
+    app.register_blueprint(views.bp)
+
+    return app
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -1,11 +1,13 @@
 from random import randrange
-from flask import redirect, render_template, url_for, abort, flash
-from opinions_app import app, db
+from flask import redirect, render_template, url_for, abort, flash, Blueprint
 from opinions_app.models import Opinion
 from opinions_app.forms import OpinionForm
+from opinions_app.extensions import db
+
+bp = Blueprint('opinions', __name__)
 
 
-@app.route('/')
+@bp.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
@@ -15,7 +17,7 @@ def index_view():
     return render_template('opinion.html', opinion=opinion)
 
 
-@app.route('/add', methods=['GET', 'POST'])
+@bp.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
@@ -30,11 +32,11 @@ def add_opinion_view():
         )
         db.session.add(opinion)
         db.session.commit()
-        return redirect(url_for('opinion_view', id=opinion.id))
+        return redirect(url_for('opinions.opinion_view', id=opinion.id))
     return render_template('add_opinion.html', form=form)
 
 
-@app.route('/opinion/<int:id>')
+@bp.route('/opinion/<int:id>')
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
